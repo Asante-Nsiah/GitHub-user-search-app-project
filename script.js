851f41cab2df0ce84ef714133c4f1273eeec47b2
Asante-Nsiah <88829934+Asante-Nsiah@users.noreply.github.com>
2022-07-22 15:01:06 +0000

    Displayed info in API

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ const url ='https://api.github.com/users/octocat'
 const infieldField = document.querySelector('.input');
 const responseField = document.querySelector('#user-id');
 const searchInfo = document.querySelector('#search');
+const joinInfo = document.querySelector('#join');
+const loginInfo = document.querySelector('#login');
+const repoInfo = document.querySelector('#repos-number');
+const followersInfo = document.querySelector('#followers-number');
+const followingInfo = document.querySelector('#following-number');   
+const locationInfo = document.querySelector('#user-location');
+const websiteInfo = document.querySelector('#user-website');
+const twitterInfo = document.querySelector('#user-twitter');
+const companyInfo = document.querySelector('#user-company');
+const profileInfo = document.querySelector('#profile-bio');
+
+
 // Asynchronous function
 const gitHubUser = async () => {
     const searchUser = infieldField.value;
@@ -24,13 +36,24 @@ const gitHubUser = async () => {
 const displaySearch = () => {
     
 gitHubUser().then((res) => {
-    responseField.innerHTML = `The ${res.login}`;
-   
-}); 
-   
 
+    responseField.innerHTML = `${res.name}`;  
+    joinInfo.innerHTML = `${'Joined '+ res.created_at.slice(0,9)}`;
+    loginInfo.innerHTML = `${'@'+ res.login}`;
+    repoInfo.innerHTML = `${res.public_repos}`;
+    followersInfo.innerHTML = `${res.followers}`;
+    followingInfo.innerHTML = `${res.following}`;
+    locationInfo.innerHTML = `${res.location}`;
+    websiteInfo.innerHTML = `${res.blog}`;
+    twitterInfo.innerHTML = `${res.twitter_username}`;
+    companyInfo.innerHTML = `${res.company}`;
+    profileInfo.innerHTML = `${res.bio}`;
+}); 
 }
 
-const textColour =  document.body.style.color ='white';
-searchInfo.addEventListener('click', displaySearch, textColour);
+// Event listener function
+
+searchInfo.addEventListener('click', displaySearch);
+
 console.log(searchInfo);
+
